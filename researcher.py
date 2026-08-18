5eef1c25664038b774ea1878f3595aed2b355fc6
thezamer <thezamerali@gmail.com>
2026-08-18 11:12:10 +0500

    Add files via upload

---
+++ researcher.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+MODEL = "openai/gpt-oss-120b"
+
+
+def ask_llm(prompt):
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
